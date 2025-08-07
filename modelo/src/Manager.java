4035b1a871947645d6e683635cced3a81ed35756
Thiago Rocha <Thiagomarlon1612@gmail.com>
2025-08-07 20:39:35 -0300

    Feat: Criar classe Manager e adiconar suas propriedades e métodos.
    - Criar classe Manager.
    - Criar suas propriedades: Login, passaword e commission.
    - Criar seus respectivos Getters/Stters

---
+++ modelo/src/Manager.java
@@ -0,0 +1,32 @@
+public class Manager {
+
+    private String login;
+
+    private String passaword;
+
+    private double commission;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassaword() {
+        return passaword;
+    }
+
+    public void setPassaword(String passaword) {
+        this.passaword = passaword;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+}
